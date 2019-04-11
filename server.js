31d1fb675b9f735d8c8dda4d04b9ea79b179623b
Kevin Yang <kkaiyang94@gmail.com>
2019-04-10 20:13:31 -0700

    Initalize server for proxy purposes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+var cors = require('cors')
+const app = express();
+const port = process.env.PORT || 3000;
+const proxy = require('http-proxy-middleware');
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use('/r/:restaurants', express.static(path.join(__dirname, 'public')));
+app.use('/', proxy({ target: 'http://localhost:3004', changeOrigin: true }))
+app.use('/', proxy({ target: 'http://localhost:3001', changeOrigin: true }))
+// app.use('/', proxy({ target: 'http://localhost:3002', changeOrigin: true }))
+// app.use('/', proxy({ target: 'http://localhost:3003', changeOrigin: true }))
+
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
