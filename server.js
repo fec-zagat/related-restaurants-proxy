e30dd3ad763b2ea6f78f77a22b4b35da9c0630fa
Kevin Yang <kkaiyang94@gmail.com>
2019-04-15 19:23:51 -0700

    Initialize server for ec2

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const proxy = require('http-proxy-middleware');
 app.use(morgan('dev'));
 app.use(cors());
 app.use('/r/:restaurants', express.static(path.join(__dirname, 'public')));
-app.use('/', proxy({ target: 'http://localhost:3004', changeOrigin: true }))
-app.use('/', proxy({ target: 'http://localhost:3001', changeOrigin: true }))
-app.use('/', proxy({ target: 'http://localhost:3002', changeOrigin: true }))
-app.use('/', proxy({ target: 'http://localhost:3003', changeOrigin: true }))
+app.use('/', proxy({ target: 'http://ec2-54-86-57-168.compute-1.amazonaws.com:3001', changeOrigin: true }))
+app.use('/', proxy({ target: 'http://ec2-52-14-95-246.us-east-2.compute.amazonaws.com:3004', changeOrigin: true }))
+app.use('/', proxy({ target: 'http://3.82.223.158:3002', changeOrigin: true }))
+app.use('/', proxy({ target: 'http://3.19.30.103:3003', changeOrigin: true }))
 
 
 app.listen(port, () => {
